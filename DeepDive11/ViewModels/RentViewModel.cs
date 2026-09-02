091bee47f185e5883fffc46bcc9212eb86275e9e
AsiyeDikenn <asiyediken@icloud.com>
2026-09-02 22:04:27 +0200

    Fået mit rentview til at bruge viewmodel istedet for models

--- DeepDive11/ViewModels/RentViewModel.cs
+++ DeepDive11/ViewModels/RentViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace DeepDive11.ViewModels
+﻿using DeepDive11.Models;
+
+namespace DeepDive11.ViewModels
 {
     public class RentViewModel
     {
+        public Products Product { get; set; }
+
+        public string? SelectedSize { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int TotalPrice { get; set; }
     }
 }
